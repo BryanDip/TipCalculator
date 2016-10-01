2112936f4b1b39003c099171509a5000daa6e1fd
Bayan <Bayan@christines-air>
2016-09-30 22:08:00 -0700

    initial commit 9/30/16

--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -9,7 +9,13 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalWithTipLabel: UILabel!
 
+    var tipValue: Double?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +25,33 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func implicitlyConvertButtonTapped(_ sender: UIButton) {
+        
+        billTextField.resignFirstResponder()
+    
+        
+        let total = Double(billTextField.text!)
+        
+        tipValue = total! * 0.15
+        
+        if let unwrappedTotal = total {
+            if let tipValueToUse = tipValue {
+                tipLabel.text = "\(tipValueToUse)"
+                let amountToPay = unwrappedTotal + tipValueToUse
+                totalWithTipLabel.text = "\(amountToPay)"
+            }
+            else {
+                tipLabel.text = "Tip is nil"
+                totalWithTipLabel.text = "\(unwrappedTotal)"
+            }
+        }
+        else {
+            print("Optional was nil!")
+        }
+    }
+        
+        
+        
 }
 
